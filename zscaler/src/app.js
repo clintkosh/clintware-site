a0fd37c4213e87ab365c79ec99d7f5c0da8d7eae
Clinton Kosh <72623901+clintkosh@users.noreply.github.com>
2026-08-12 11:10:09 -0500

    Add ZS application bundle loader

---
+++ zscaler/src/app.js
@@ -0,0 +1,6 @@
+import { PART_1 } from "./app-part1.js";
+import { PART_2 } from "./app-part2.js";
+import { PART_3 } from "./app-part3.js";
+import { PART_4 } from "./app-part4.js";
+import { PART_5 } from "./app-part5.js";
+export const APP_GZ_B64 = [PART_1,PART_2,PART_3,PART_4,PART_5].join("");
