fc22b572e4a6c9e3cbf4a332f8fc81a19802bbae
Clinton Kosh <72623901+clintkosh@users.noreply.github.com>
2026-09-18 17:24:45 -0500

    Allow GA4 endpoints on ClintCal

--- meet-worker/src/worker.js
+++ meet-worker/src/worker.js
@@ -36,7 +36,7 @@ function html(content) {
     headers: securityHeaders({
       "Content-Type": "text/html; charset=utf-8",
       "Cache-Control": "no-store",
-      "Content-Security-Policy": "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; connect-src 'self'; img-src 'self' data:; font-src 'self' data:; base-uri 'none'; frame-ancestors 'none'; form-action 'self';",
+      "Content-Security-Policy": "default-src 'self'; script-src 'self' 'unsafe-inline' https://www.googletagmanager.com; style-src 'self' 'unsafe-inline'; connect-src 'self' https://www.google-analytics.com https://analytics.google.com https://region1.google-analytics.com; img-src 'self' data: https://www.google-analytics.com; font-src 'self' data:; base-uri 'none'; frame-ancestors 'none'; form-action 'self';",
     }),
   });
 }
