6746515aa3d635e6a9f8f4ccb94cad6d7fa360a5
Clinton Kosh <72623901+clintkosh@users.noreply.github.com>
2026-09-22 03:16:03 -0500

    Add shared Clintware analytics to N7

--- projects/n7-customer-value-os/src/routes/__root.tsx
+++ projects/n7-customer-value-os/src/routes/__root.tsx
@@ -110,6 +110,13 @@ function RootShell({ children }: { children: ReactNode }) {
     <html lang="en">
       <head>
         <HeadContent />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-DCY144YM9P" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html:
+              "window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments)}gtag('js',new Date());gtag('config','G-DCY144YM9P',{anonymize_ip:true});",
+          }}
+        />
       </head>
       <body>
         {children}
